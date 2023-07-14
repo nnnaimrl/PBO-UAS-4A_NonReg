1a2230e5af4478c5b8f64534a1880dcc4d7a7c13
nnnaimrl <naimaaaibrahim@gmail.com>
2023-07-15 06:20:18 +0800

    Inisialisasi Proyek dan Kelas Penduduk

---
+++ src/Penduduk.java
@@ -0,0 +1,23 @@
+class Penduduk {
+    private String nama;
+    private int umur;
+    private String alamat;
+
+    public Penduduk(String nama, int umur, String alamat) {
+        this.nama = nama;
+        this.umur = umur;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+}
